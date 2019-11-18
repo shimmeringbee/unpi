f3c44fcc461aca2aa86ce6bb2bfef9c709aa0380
Peter Wood <peter@alastria.net>
2019-11-18 20:07:36 +0000

    When not using iota, we need to specify types manually.

--- frame.go
+++ frame.go
@@ -14,38 +14,38 @@ type MessageType byte
 
 const (
 	POLL MessageType = 0x00
-	SREQ             = 0x01
-	AREQ             = 0x02
-	SRSP             = 0x03
+	SREQ MessageType = 0x01
+	AREQ MessageType = 0x02
+	SRSP MessageType = 0x03
 )
 
 type Subsystem byte
 
 const (
 	RES0        Subsystem = 0x00
-	SYS                   = 0x01
-	MAC                   = 0x02
-	NWK                   = 0x03
-	AF                    = 0x04
-	ZDO                   = 0x05
-	SAPI                  = 0x06
-	UTIL                  = 0x07
-	DBG                   = 0x08
-	APP                   = 0x09
-	RCAF                  = 0x0a
-	RCN                   = 0x0b
-	RCN_CLIENT            = 0x0c
-	BOOT                  = 0x0d
-	ZIPTEST               = 0x0e
-	DEBUG                 = 0x0f
-	PERIPHERALS           = 0x10
-	NFC                   = 0x11
-	PB_NWK_MGR            = 0x12
-	PB_GW                 = 0x13
-	PB_OTA_MGR            = 0x14
-	BLE_SPNP              = 0x15
-	BLE_HCI               = 0x16
-	SRV_CTR               = 0x1f
+	SYS         Subsystem = 0x01
+	MAC         Subsystem = 0x02
+	NWK         Subsystem = 0x03
+	AF          Subsystem = 0x04
+	ZDO         Subsystem = 0x05
+	SAPI        Subsystem = 0x06
+	UTIL        Subsystem = 0x07
+	DBG         Subsystem = 0x08
+	APP         Subsystem = 0x09
+	RCAF        Subsystem = 0x0a
+	RCN         Subsystem = 0x0b
+	RCN_CLIENT  Subsystem = 0x0c
+	BOOT        Subsystem = 0x0d
+	ZIPTEST     Subsystem = 0x0e
+	DEBUG       Subsystem = 0x0f
+	PERIPHERALS Subsystem = 0x10
+	NFC         Subsystem = 0x11
+	PB_NWK_MGR  Subsystem = 0x12
+	PB_GW       Subsystem = 0x13
+	PB_OTA_MGR  Subsystem = 0x14
+	BLE_SPNP    Subsystem = 0x15
+	BLE_HCI     Subsystem = 0x16
+	SRV_CTR     Subsystem = 0x1f
 )
 
 type Frame struct {
