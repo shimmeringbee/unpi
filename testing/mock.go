f69d74937bb76d48759e98f0cc2b83c2fd573647
Peter Wood <peter@alastria.net>
2019-11-23 16:43:18 +0000

    Add mocking library for a unpi client, with return values and assertions.

---
+++ testing/mock.go
@@ -0,0 +1,212 @@
+package testing
+
+import (
+	"bytes"
+	. "github.com/shimmeringbee/unpi"
+	"io"
+	"math"
+	"sync/atomic"
+	"testing"
+)
+
+type MockAdapter struct {
+	sequencer *int64
+
+	ReceivedFrames []Frame
+
+	incomingReader io.Reader
+	incomingWriter io.WriteCloser
+	incomingEnd    chan bool
+
+	outgoingBuffer *bytes.Buffer
+	outgoingFrames chan Frame
+	outgoingEnd    chan bool
+
+	Calls []*Call
+}
+
+type CallRecord struct {
+	when  int64
+	Frame Frame
+}
+
+func (cr CallRecord) Before(ocr CallRecord) bool {
+	return cr.when < ocr.when
+}
+
+func (cr CallRecord) AssertBefore(t *testing.T, ocr CallRecord) {
+	if !cr.Before(ocr) {
+		t.Logf("assertion failed, call happened after")
+		t.Fail()
+	}
+}
+
+func (cr CallRecord) After(ocr CallRecord) bool {
+	return cr.when > ocr.when
+}
+
+func (cr CallRecord) AssertAfter(t *testing.T, ocr CallRecord) {
+	if !cr.After(ocr) {
+		t.Logf("assertion failed, call happened before")
+		t.Fail()
+	}
+}
+
+type Call struct {
+	sequencer *int64
+
+	mT MessageType
+	s  Subsystem
+	c  byte
+
+	CapturedCalls []CallRecord
+	returnFrames  []Frame
+
+	expectedCalls int
+	actualCalls   int
+}
+
+func (c *Call) Return(frames ...Frame) *Call {
+	c.returnFrames = frames
+	return c
+}
+
+func (c *Call) Times(times int) *Call {
+	c.expectedCalls = times
+	return c
+}
+
+func (c *Call) UnlimitedTimes() *Call {
+	c.expectedCalls = math.MinInt64
+	return c
+}
+
+func (c *Call) Frames() []Frame {
+	return []Frame{}
+}
+
+func NewMockAdapter() *MockAdapter {
+	m := &MockAdapter{
+		sequencer:      new(int64),
+		ReceivedFrames: []Frame{},
+
+		incomingEnd: make(chan bool, 1),
+
+		Calls: []*Call{},
+
+		outgoingFrames: make(chan Frame, 50),
+		outgoingEnd:    make(chan bool, 1),
+	}
+
+	*m.sequencer = 0
+	m.incomingReader, m.incomingWriter = io.Pipe()
+
+	m.start()
+
+	return m
+}
+
+func (m *MockAdapter) Read(p []byte) (n int, err error) {
+	if m.outgoingBuffer == nil {
+		select {
+		case f := <-m.outgoingFrames:
+			data := f.Marshall()
+			m.outgoingBuffer = bytes.NewBuffer(data)
+		case <-m.outgoingEnd:
+			return 0, io.EOF
+		}
+	}
+
+	actualRead, err := m.outgoingBuffer.Read(p)
+
+	if m.outgoingBuffer.Len() == 0 {
+		m.outgoingBuffer = nil
+	}
+
+	return actualRead, err
+}
+
+func (m *MockAdapter) Write(p []byte) (n int, err error) {
+	return m.incomingWriter.Write(p)
+}
+
+const (
+	AnyType      MessageType = 0xff
+	AnySubsystem Subsystem   = 0xff
+	AnyCommand   uint8       = 0xff
+)
+
+func (m *MockAdapter) On(mT MessageType, s Subsystem, c uint8) *Call {
+	call := &Call{
+		sequencer:     m.sequencer,
+		mT:            mT,
+		s:             s,
+		c:             c,
+		CapturedCalls: []CallRecord{},
+		returnFrames:  []Frame{},
+		expectedCalls: 1,
+		actualCalls:   0,
+	}
+
+	m.Calls = append(m.Calls, call)
+
+	return call
+}
+
+func (m *MockAdapter) AssertCalls(t *testing.T) {
+	for _, call := range m.Calls {
+		if call.expectedCalls != call.actualCalls && call.expectedCalls != math.MinInt64 {
+			t.Logf("call count mismatch (mT: %v s: %v c: %v): expected (%d) != actual(%d)", call.mT, call.s, call.c, call.expectedCalls, call.actualCalls)
+			t.Fail()
+		}
+	}
+}
+
+func (m *MockAdapter) handleIncoming() {
+	for {
+		frame, err := Read(m.incomingReader)
+		if err != nil {
+			return
+		}
+
+		m.ReceivedFrames = append(m.ReceivedFrames, frame)
+		m.matchCalls(frame)
+
+		select {
+		case <-m.incomingEnd:
+			return
+		default:
+		}
+	}
+}
+
+func (m *MockAdapter) matchCalls(frame Frame) {
+	for _, call := range m.Calls {
+		if (call.mT == frame.MessageType || call.mT == AnyType) &&
+			(call.s == frame.Subsystem || call.s == AnySubsystem) &&
+			(call.c == frame.CommandID || call.c == AnyCommand) {
+
+			if len(call.returnFrames) > 0 {
+				i := call.actualCalls % len(call.returnFrames)
+				m.outgoingFrames <- call.returnFrames[i]
+			}
+
+			call.CapturedCalls = append(call.CapturedCalls, CallRecord{
+				when:  atomic.AddInt64(call.sequencer, 1),
+				Frame: frame,
+			})
+
+			call.actualCalls += 1
+		}
+	}
+}
+
+func (m *MockAdapter) start() {
+	go m.handleIncoming()
+}
+
+func (m *MockAdapter) Stop() {
+	_ = m.incomingWriter.Close()
+	m.incomingEnd <- true
+	m.outgoingEnd <- true
+}
---
+++ testing/mock_test.go
@@ -0,0 +1,253 @@
+package testing
+
+import (
+	. "github.com/shimmeringbee/unpi"
+	"github.com/stretchr/testify/assert"
+	"io"
+	"testing"
+	"time"
+)
+
+func TestMockAdapter_ReceivingFrames(t *testing.T) {
+	t.Run("written frames have been stored in the mock", func(t *testing.T) {
+		m := NewMockAdapter()
+		defer m.Stop()
+
+		expectedFrame := Frame{MessageType: SREQ, Subsystem: ZDO, CommandID: 0xf0, Payload: []byte{0x02, 0x11}}
+
+		err := Write(m, expectedFrame)
+		assert.NoError(t, err)
+
+		err = Write(m, expectedFrame)
+		assert.NoError(t, err)
+
+		assert.Equal(t, 2, len(m.ReceivedFrames))
+		assert.Equal(t, expectedFrame, m.ReceivedFrames[0])
+		assert.Equal(t, expectedFrame, m.ReceivedFrames[1])
+	})
+}
+
+func TestMockAdapter(t *testing.T) {
+	t.Run("single mocked response return their correct Frame", func(t *testing.T) {
+		m := NewMockAdapter()
+		defer m.Stop()
+
+		expectedFrame := Frame{MessageType: SRSP, Subsystem: ZDO, CommandID: 0xf0, Payload: []byte{0x00}}
+		m.On(SREQ, ZDO, 0xf0).Return(expectedFrame)
+
+		frame := Frame{MessageType: SREQ, Subsystem: ZDO, CommandID: 0xf0, Payload: []byte{0x02, 0x11}}
+
+		err := Write(m, frame)
+		assert.NoError(t, err)
+
+		actualFrame, err := Read(m)
+		assert.Equal(t, expectedFrame, actualFrame)
+
+		internalT := new(testing.T)
+		m.AssertCalls(internalT)
+
+		assert.False(t, internalT.Failed())
+	})
+
+	t.Run("mocked response with no return frame does nothing but record", func(t *testing.T) {
+		m := NewMockAdapter()
+
+		m.On(SREQ, ZDO, 0xf0)
+
+		frame := Frame{MessageType: SREQ, Subsystem: ZDO, CommandID: 0xf0, Payload: []byte{0x02, 0x11}}
+
+		err := Write(m, frame)
+		assert.NoError(t, err)
+
+		// Force stop to ensure the Read results in an EOF
+		m.Stop()
+
+		_, err = Read(m)
+		assert.Error(t, err)
+		assert.Equal(t, io.EOF, err)
+
+		internalT := new(testing.T)
+		m.AssertCalls(internalT)
+
+		assert.False(t, internalT.Failed())
+	})
+
+	t.Run("single mocked response errors if not called", func(t *testing.T) {
+		m := NewMockAdapter()
+		defer m.Stop()
+
+		expectedFrame := Frame{MessageType: SRSP, Subsystem: ZDO, CommandID: 0xf0, Payload: []byte{0x00}}
+		m.On(SREQ, ZDO, 0xf0).Return(expectedFrame)
+
+		internalT := new(testing.T)
+		m.AssertCalls(internalT)
+
+		assert.True(t, internalT.Failed())
+	})
+
+	t.Run("single mocked response return their correct frame if repeated, though assert fails due to only one call allowed", func(t *testing.T) {
+		m := NewMockAdapter()
+		defer m.Stop()
+
+		expectedFrame := Frame{MessageType: SRSP, Subsystem: ZDO, CommandID: 0xf0, Payload: []byte{0x00}}
+		m.On(SREQ, ZDO, 0xf0).Return(expectedFrame)
+
+		frame := Frame{MessageType: SREQ, Subsystem: ZDO, CommandID: 0xf0, Payload: []byte{0x02, 0x11}}
+
+		err := Write(m, frame)
+		assert.NoError(t, err)
+
+		actualFrame, err := Read(m)
+		assert.Equal(t, expectedFrame, actualFrame)
+
+		err = Write(m, frame)
+		assert.NoError(t, err)
+
+		actualFrame, err = Read(m)
+		assert.Equal(t, expectedFrame, actualFrame)
+
+		internalT := new(testing.T)
+		m.AssertCalls(internalT)
+
+		assert.True(t, internalT.Failed())
+	})
+
+	t.Run("single mocked response return their correct frame if repeated, multiple calls allowed", func(t *testing.T) {
+		m := NewMockAdapter()
+		defer m.Stop()
+
+		expectedFrame := Frame{MessageType: SRSP, Subsystem: ZDO, CommandID: 0xf0, Payload: []byte{0x00}}
+		m.On(SREQ, ZDO, 0xf0).Return(expectedFrame).Times(2)
+
+		frame := Frame{MessageType: SREQ, Subsystem: ZDO, CommandID: 0xf0, Payload: []byte{0x02, 0x11}}
+
+		err := Write(m, frame)
+		assert.NoError(t, err)
+
+		actualFrame, err := Read(m)
+		assert.Equal(t, expectedFrame, actualFrame)
+
+		err = Write(m, frame)
+		assert.NoError(t, err)
+
+		actualFrame, err = Read(m)
+		assert.Equal(t, expectedFrame, actualFrame)
+
+		internalT := new(testing.T)
+		m.AssertCalls(internalT)
+
+		assert.False(t, internalT.Failed())
+	})
+
+	t.Run("single mocked response return their correct frame if repeated, unlimited calls allowed", func(t *testing.T) {
+		m := NewMockAdapter()
+		defer m.Stop()
+
+		expectedFrame := Frame{MessageType: SRSP, Subsystem: ZDO, CommandID: 0xf0, Payload: []byte{0x00}}
+		m.On(SREQ, ZDO, 0xf0).Return(expectedFrame).UnlimitedTimes()
+
+		frame := Frame{MessageType: SREQ, Subsystem: ZDO, CommandID: 0xf0, Payload: []byte{0x02, 0x11}}
+
+		err := Write(m, frame)
+		assert.NoError(t, err)
+
+		actualFrame, err := Read(m)
+		assert.Equal(t, expectedFrame, actualFrame)
+
+		err = Write(m, frame)
+		assert.NoError(t, err)
+
+		actualFrame, err = Read(m)
+		assert.Equal(t, expectedFrame, actualFrame)
+
+		internalT := new(testing.T)
+		m.AssertCalls(internalT)
+
+		assert.False(t, internalT.Failed())
+	})
+
+	t.Run("single mocked response with multiple returns will return the correct Frame if repeated", func(t *testing.T) {
+		m := NewMockAdapter()
+		defer m.Stop()
+
+		expectedFrameOne := Frame{MessageType: SRSP, Subsystem: ZDO, CommandID: 0xf0, Payload: []byte{0x00}}
+		expectedFrameTwo := Frame{MessageType: SRSP, Subsystem: ZDO, CommandID: 0xf0, Payload: []byte{0x01}}
+
+		m.On(SREQ, ZDO, 0xf0).Return(expectedFrameOne, expectedFrameTwo).Times(2)
+
+		frame := Frame{MessageType: SREQ, Subsystem: ZDO, CommandID: 0xf0, Payload: []byte{0x02, 0x11}}
+
+		err := Write(m, frame)
+		assert.NoError(t, err)
+
+		actualFrame, err := Read(m)
+		assert.Equal(t, expectedFrameOne, actualFrame)
+
+		err = Write(m, frame)
+		assert.NoError(t, err)
+
+		actualFrame, err = Read(m)
+		assert.Equal(t, expectedFrameTwo, actualFrame)
+
+		internalT := new(testing.T)
+		m.AssertCalls(internalT)
+
+		assert.False(t, internalT.Failed())
+	})
+
+	t.Run("single mocked response return their correct frame if wild carded", func(t *testing.T) {
+		m := NewMockAdapter()
+		defer m.Stop()
+
+		expectedFrame := Frame{MessageType: SRSP, Subsystem: ZDO, CommandID: 0xf0, Payload: []byte{0x00}}
+		m.On(AnyType, AnySubsystem, AnyCommand).Return(expectedFrame)
+
+		frame := Frame{MessageType: SREQ, Subsystem: ZDO, CommandID: 0xf0, Payload: []byte{0x02, 0x11}}
+
+		err := Write(m, frame)
+		assert.NoError(t, err)
+
+		actualFrame, err := Read(m)
+		assert.Equal(t, expectedFrame, actualFrame)
+
+		internalT := new(testing.T)
+		m.AssertCalls(internalT)
+
+		assert.False(t, internalT.Failed())
+	})
+
+	t.Run("single mocked response with multiple returns will return the correct Frame if repeated", func(t *testing.T) {
+		m := NewMockAdapter()
+
+		c := m.On(SREQ, ZDO, 0xf0).Times(2)
+
+		frame := Frame{MessageType: SREQ, Subsystem: ZDO, CommandID: 0xf0, Payload: []byte{0x02, 0x11}}
+
+		err := Write(m, frame)
+		assert.NoError(t, err)
+
+		err = Write(m, frame)
+		assert.NoError(t, err)
+
+		time.Sleep(5 * time.Millisecond)
+
+		m.Stop()
+
+		assert.Equal(t, 2, len(c.CapturedCalls))
+
+		assert.Equal(t, frame, c.CapturedCalls[0].Frame)
+		assert.Equal(t, frame, c.CapturedCalls[1].Frame)
+
+		assert.True(t, c.CapturedCalls[0].Before(c.CapturedCalls[1]))
+
+		internalT := new(testing.T)
+		c.CapturedCalls[0].AssertBefore(internalT, c.CapturedCalls[1])
+		assert.False(t, internalT.Failed())
+
+		assert.True(t, c.CapturedCalls[1].After(c.CapturedCalls[0]))
+
+		internalT = new(testing.T)
+		c.CapturedCalls[1].AssertAfter(internalT, c.CapturedCalls[0])
+		assert.False(t, internalT.Failed())
+	})
+}
